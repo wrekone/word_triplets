5f1291471f71f3fcad838c01742bfbbe9c839a5a
wrekone <wrekone@users.noreply.github.com>
2019-09-13 13:24:53 -0700

    Updated line spacing per PEP8.

--- word_triplets.py
+++ word_triplets.py
@@ -1,5 +1,6 @@
 import word_triplets_processor
 
+
 words = word_triplets_processor.text_input()
 words_list = word_triplets_processor.text_transform(words)
 words_triples = word_triplets_processor.text_triple_maker(words_list)
