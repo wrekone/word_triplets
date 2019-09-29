bd90afa11c04bbe4ff48f7ee5cebb439b66126a0
wrekone <wrekone@users.noreply.github.com>
2019-09-29 12:51:09 -0700

    Change variable names.

--- word_triplets.py
+++ word_triplets.py
@@ -2,7 +2,7 @@ import word_triplets_processor
 
 
 words = word_triplets_processor.text_input()
-words_list = word_triplets_processor.text_transform(words)
-words_triples = word_triplets_processor.text_triple_maker(words_list)
-output = word_triplets_processor.group_count(words_triples)
+word_list = word_triplets_processor.text_transform(words)
+word_triples = word_triplets_processor.text_triple_maker(word_list)
+output = word_triplets_processor.group_count(word_triples)
 print(output)
